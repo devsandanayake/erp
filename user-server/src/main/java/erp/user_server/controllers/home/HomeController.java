690267230d4509efd76c467f2143370049c136e6
devin <devinsandanayake49@gmail.com>
2025-10-07 09:24:14 +0530

    update

--- user-server/src/main/java/erp/user_server/controllers/home/HomeController.java
+++ user-server/src/main/java/erp/user_server/controllers/home/HomeController.java
@@ -1,4 +1,4 @@
-package main.java.erp.user_server.controllers.home;
+package erp.user_server.controllers.home;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
